ea507c677e0c5b40719381d09cf3753784bc418f
Song Xia <sx801@nyu.edu>
2022-12-04 09:41:21 -0500

    fix a incorrect file name

--- bash_scripts/download_models_and_extract.bash
+++ bash_scripts/download_models_and_extract.bash
@@ -6,7 +6,7 @@ cd ./data/models
 wget https://yzhang.hpc.nyu.edu/IMA/Datasets/sPhysNet-MT_cal_trained_models.tar.gz
 wget https://yzhang.hpc.nyu.edu/IMA/Datasets/sPhysNet-MT_exp_trained_models.tar.gz
 
-tar xvf cal_trained_models.tar.gz
+tar xvf sPhysNet-MT_cal_trained_models.tar.gz
 
 ## extract models trained on the experimental dataset. Since 50 random splits are trained, we only extract the first split for demostration
-tar xvf exp_trained_models.tar.gz --wildcards exp_ultimate_freeSolv_13_RDrun_2022-05-20_100307__201005/exp_ultimate_freeSolv_13_active_ALL_2022-05-20_100309
+tar xvf sPhysNet-MT_exp_trained_models.tar.gz --wildcards exp_ultimate_freeSolv_13_RDrun_2022-05-20_100307__201005/exp_ultimate_freeSolv_13_active_ALL_2022-05-20_100309
